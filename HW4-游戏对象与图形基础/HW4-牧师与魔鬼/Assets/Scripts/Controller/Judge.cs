91a2e916c0bb11f6b0dd552d8189a1e375cd0b30
wuzy57 <1120902512@qq.com>
2019-10-09 14:59:39 +0800

    HW4.4

---
+++ HW4-游戏对象与图形基础/HW4-牧师与魔鬼/Assets/Scripts/Controller/Judge.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Judge : MonoBehaviour
+{
+    public int icheck_game_status(BoatModel boat, CoastModel to_Coast, CoastModel from_Coast)
+    {
+        int from_priest = 0;
+        int from_devil = 0;
+        int to_priest = 0;
+        int to_devil = 0;
+
+        int[] fromCount = from_Coast.getCharacterNum();
+        from_priest += fromCount[0];
+        from_devil += fromCount[1];
+
+        int[] toCount = to_Coast.getCharacterNum();
+        to_priest += toCount[0];
+        to_devil += toCount[1];
+
+        //胜利
+        if (to_priest + to_devil == 6)
+            return 2;
+
+        int[] boatCount = boat.getCharacterNum();
+
+        //游戏继续
+        //船在对岸
+        if (boat.get_to_from() == -1)
+        {
+            to_priest += boatCount[0];
+            to_devil += boatCount[1];
+        }
+        //船在初始岸
+        else
+        {
+            from_priest += boatCount[0];
+            from_devil += boatCount[1];
+        }
+
+        //失败
+        if (from_priest < from_devil && from_priest > 0)
+        {
+            return 1;
+        }
+        if (to_priest < to_devil && to_priest > 0)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+}
---
+++ HW4-游戏对象与图形基础/HW4-牧师与魔鬼/Assets/Scripts/Controller/SSActionManager.cs
@@ -0,0 +1,52 @@
+﻿//动作管理基类 – SSActionManager
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SSActionManager : MonoBehaviour
+{
+    public Judge jd;
+    public void icharacterIsClicked(CharacterModel characterCtrl,BoatModel boat,CoastModel to_Coast,CoastModel from_Coast, UserGUI userGUI)
+    {
+        if (characterCtrl.isOnBoat())
+        {
+            CoastModel whichCoast;
+            // -1to、1from
+            if (boat.get_to_from() == -1)
+            {
+                whichCoast = to_Coast;
+            }
+            else
+            {
+                whichCoast = from_Coast;
+            }
+
+            boat.GetOffBoat(characterCtrl.getName());
+            characterCtrl.moveToPosition(whichCoast.getEmptyPosition());
+            characterCtrl.getOnCoast(whichCoast);
+            whichCoast.getOnCoast(characterCtrl);
+
+        }
+        else//Character在岸上
+        {
+            CoastModel whichCoast = characterCtrl.getCoastController();
+
+            //船是满的
+            if (boat.getEmptyNum() == -1)
+            {
+                return;
+            }
+
+            //船和目标岸位置不同
+            if (whichCoast.get_to_or_from() != boat.get_to_from())
+                return;
+
+            whichCoast.getOffCoast(characterCtrl.getName());
+            characterCtrl.moveToPosition(boat.getEmptyPosition());
+            characterCtrl.getOnBoat(boat);
+            boat.GetOnBoat(characterCtrl);
+        }
+        userGUI.status = jd.icheck_game_status(boat,to_Coast, from_Coast);
+    }
+
+}
