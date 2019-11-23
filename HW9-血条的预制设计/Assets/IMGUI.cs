c9deb75a6fb12b46e285ebed0babd9328938cb9c
wuzy57 <1120902512@qq.com>
2019-11-23 19:56:51 +0800

    hw9

---
+++ HW9-血条的预制设计/Assets/IMGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IMGUI : MonoBehaviour
+{
+    private float hSliderValue = 100f;
+    private void OnGUI()
+    {
+        Vector3 worldPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        hSliderValue = GUI.HorizontalSlider(new Rect(screenPos.x - 100, screenPos.y, 100, 100), hSliderValue, 0.0f, 100.0f);
+    }
+}
---
+++ HW9-血条的预制设计/README.md
@@ -0,0 +1,3 @@
+#### HW9-血条的预制设计
+
+[博客](https://blog.csdn.net/wzycxy/article/details/103217924)
